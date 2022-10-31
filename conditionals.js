8e48ca85c54dae921df229dbb92cf199a6a65551
open-meadow <rohan.varughese@protonmail.com>
2022-10-31 19:22:26 -0400

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,13 @@
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+//new block
+
 const temperature = 12;
 
 if (temperature < 0) {
@@ -9,3 +19,19 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+// new block 
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+//
+
+let raining = true;
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
